40cffd57f9b85f4867c03d6daa37e02d11398acb
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-02-26 18:27:55 +0300

    refactor: response-sender

--- server/components/application/app-response/response-sender.js
+++ server/components/application/app-response/response-sender.js
@@ -14,23 +14,25 @@ class ResponseSender {
 
   async _tryToSend(getResponse, response) {
     const {code, headers, filename} = getResponse;
-    console.log('code', code);
+    
     response.writeHead(code, headers);
     await this._sendFiledata(filename, response);
   }
   async _sendFiledata(filename, response) {
-    const readStream = new FS.createReadStream(filename);
-    readStream.on('readable', () => {
-      const data = readStream.read();
-      if( data !== null ) response.write(data);
-    });
-    readStream.on('end', () => {
-      response.end();
-    });
-    readStream.on('error', (error) => {
-      console.log('send file error: ', error);
-      response.end();
-    });
+    const readStream = new FS.ReadStream(filename);
+
+    readStream
+      .on('readable', () => {
+        const data = readStream.read();
+        if( data !== null ) response.write(data);
+      })
+      .on('end', () => {
+        response.end();
+      })
+      .on('error', (error) => {
+        console.log('send file error: ', error);
+        response.end();
+      });
   }
 }
 
