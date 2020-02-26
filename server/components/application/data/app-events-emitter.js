4f306ac688822abb8918afa4857eefd0d7170802
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-02-26 10:54:33 +0300

    create: create app-events-emitter

---
+++ server/components/application/data/app-events-emitter.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const EventEmitter = require('events');
+
+class AppEventsEmitter extends EventEmitter {}
+const appEventsEmitter = new AppEventsEmitter();
+
+module.exports = appEventsEmitter;
