2f4c4ab6222a6c2c1d2f1691f8fb74e1d8b3ecb7
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-02-26 12:35:39 +0300

    create: url-parser test

--- server/components/application/request-parsers/tests/url-parser.test.js
+++ server/components/application/request-parsers/tests/url-parser.test.js
@@ -1,14 +1,31 @@
 'use strict';
 
-const mocha = require('mocha');
 const assert = require('assert');
+const urlParser = {
+  parse(url) {
+    return {path: ''};
+  }
+};
 
 describe('URLParser class test', () => {
-  it('parse url = \'/\'. Expect result: \'/1\'', () => {
+  it('parse url = \'/\'', () => {
     const url = '/';
+    const parsedURL = urlParser.parse(url);
     
-    url += '1';
+    assert.equal(parsedURL.path, '/');
+  });
 
-    assert.equal(url, '/1');
+  it('parse url = \'/filename.js\'', () => {
+    const url = '/filename.js';
+    const parsedURL = urlParser.parse(url);
+    
+    assert.equal(parsedURL.path, '/filename.js');
+  });
+
+  it('parse url = \'/dirname/filename.js\'', () => {
+    const url = '/dirname/filename.js';
+    const parsedURL = urlParser.parse(url);
+    
+    assert.equal(parsedURL.path, '/dirname/filename.js');
   });
 });
