5227213724c509f13df8b72112cbd370e0ae71f0
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-01 19:58:37 +0300

    update: added entity schemas to validation

--- server/components/application/validation/src/entity-schema.js
+++ server/components/application/validation/src/entity-schema.js
@@ -1,6 +1,12 @@
 'use strict';
 
-const schemas = {};
+const userValidationSchema = require('../../../user/data/validation-schema');
+const projectValidationSchema = require('../../../project/data/validation-schema');
+
+const schemas = {
+  user: userValidationSchema,
+  project: projectValidationSchema
+};
 
 function getEntitySchema(entity) {
   return schemas[entity];
