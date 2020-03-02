dbadae6c03ec044a8e02b6b838c0bd2301efebeb
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-02 22:42:43 +0300

    create: project-repository implementation

---
+++ server/components/repository/project-repository-impl.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const projectsDB = require('../../../db/projects.json');
+const IProjectRepository = require('../project/repository/i-project-repository');
+
+class ProjectRepositoryImpl {
+  async getProjects(accessID) {
+    return projectsDB[accessID];
+  }
+}
+
+function implementProjectRepository() {
+  const projectRepositoryImpl = new ProjectRepositoryImpl;
+  IProjectRepository.getProjects = projectRepositoryImpl.getProjects;
+}
+
+module.exports = implementProjectRepository;
