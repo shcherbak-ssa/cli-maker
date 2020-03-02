ab4a800470e3d7284bcb4fde7999343952629010
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-02 20:19:55 +0300

    create: user-data classes

---
+++ server/components/user/data/user-data.js
@@ -0,0 +1,43 @@
+'use strict';
+
+class UserDataType {
+  constructor() {
+    this.name = '';
+    this.userID = '';
+  }
+}
+
+class UserData {
+  constructor(userDataType) {
+    this._data = userDataType;
+  }
+
+  getName() {
+    return this._data.name;
+  }
+  getUserID() {
+    return this._data.userID;
+  }
+  getData() {
+    return this._data;
+  }
+}
+
+class UserDataCreator {
+  constructor() {
+    this._data = new UserDataType();
+  }
+
+  setName(name) {
+    this._data.name = name;
+    return this;
+  }
+  setUserID(userID) {
+    this._data.userID = userID;
+  }
+  getUserData() {
+    return new UserData(this._data);
+  }
+}
+
+module.exports = UserDataCreator;
