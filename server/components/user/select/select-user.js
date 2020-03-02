ffbbc7930ec9c779daefbfaf11a370ffddc840a5
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-02 21:02:44 +0300

    draft: select-user class

---
+++ server/components/user/select/select-user.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const SelectUserDataCreator = require('../data/select-user-data');
+
+class SelectUser {
+  async handler(workingData) {
+    try {
+      return await this._tryToRunHandler(workingData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async _tryToRunHandler(workingData) {
+    const accessID = workingData.getAccessID();
+    const SelectUserData = await this._getSelectUserDataFromRepository(accessID);
+  }
+  async _getSelectUserDataFromRepository(accessID) {}
+}
+
+const selectUser = new SelectUser();
+module.exports = selectUser;
