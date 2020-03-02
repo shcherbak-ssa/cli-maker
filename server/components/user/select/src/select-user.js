9c4d695f05e72bba6f9685fb001a0099229f5dcf
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-02 21:36:37 +0300

    create: select-user class

--- server/components/user/select/src/select-user.js
+++ server/components/user/select/src/select-user.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const repostioryWorker = require('./repository-worker');
+const repositoryWorker = require('./repository-worker');
 const SelectUserDataCreator = require('../../data/select-user-data');
+const userResponseWorker = require('../../data/user-response-worker');
 
 class SelectUser {
   async handler(workingData) {
@@ -14,9 +15,21 @@ class SelectUser {
 
   async _tryToRunHandler(workingData) {
     const accessID = workingData.getAccessID();
-    const SelectUserData = await this._getSelectUserDataFromRepository(accessID);
+    const selectUserData = await this._getSelectUserDataFromRepository(accessID);
+    const data = selectUserData.getSelectUserData();
+
+    return userResponseWorker.createSuccessResponse(data);
   }
-  async _getSelectUserDataFromRepository(accessID) {}
+  async _getSelectUserDataFromRepository(accessID) {
+    const userDataType = await repositoryWorker.getUserData(accessID);
+    const projects = await repositoryWorker.getProjects(accessID);
+    const creator = new SelectUserDataCreator();
+
+    return creator
+      .setUserData(userDataType)
+      .setProjects(projects)
+      .getSelectData();
+  }
 }
 
 const selectUser = new SelectUser();
