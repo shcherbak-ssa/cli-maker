66d683513d92f8914f18b15065adaf0bf917c777
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-03 10:52:47 +0300

    create: user component launcher

---
+++ server/components/user/launcher.js
@@ -0,0 +1,5 @@
+'use strict';
+
+function initUserComponent() {}
+
+module.exports = initUserComponent;
--- server/launcher.js
+++ server/launcher.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const AppServer = require('./components/application/server');
+const initUserComponent = require('./components/user/launcher');
 const initProjectComponent = require('./components/project/launcher');
 const initRepositoryComponent = require('./components/repository/launcher');
 const initEventsComponent = require('./components/events/launcher');
@@ -21,6 +22,7 @@ class AppLauncher {
     appServer.run();
   }
   _initComponents() {
+    initUserComponent();
     initProjectComponent();
     initRepositoryComponent();
     initEventsComponent();
