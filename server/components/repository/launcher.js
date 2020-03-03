0801dcb24d9b73b821c959fee3201753ff526343
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-03 20:45:24 +0300

    update: repository component launcher

--- server/components/repository/launcher.js
+++ server/components/repository/launcher.js
@@ -2,10 +2,12 @@
 
 const implementUserRepository = require('./user-repository-impl');
 const implementProjectRepository = require('./project-repository-impl');
+const implementProjectUseCasesRepositories = require('./project-use-case-repository-impl');
 
 function initRepositoryComponent() {
   implementUserRepository();
   implementProjectRepository();
+  implementProjectUseCasesRepositories();
 }
 
 module.exports = initRepositoryComponent;
