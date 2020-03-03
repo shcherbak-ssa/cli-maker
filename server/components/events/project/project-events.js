c955310cb658a2e262f361ab8a87a010278c7f26
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-03 14:38:01 +0300

    draft: project-events class

---
+++ server/components/events/project/project-events.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const appEventsEmitter = require('../../application/data/app-events-emitter');
+const workingDataCreator = require();
+const selectProject = require('../../project/select/select-project');
+
+class ProjectEvents {
+  initProjectEvents() {
+    appEventsEmitter
+      .on('select-project', this._selectProjectEventHandler);
+  }
+
+  async _selectProjectEventHandler(requestBody, responseCallback) {
+    const workingData = workingDataCreator.createSelectProjectWorkingData(requestBody);
+    const projectResponse = await selectProject.handler(workingData);
+    await responseCallback(projectResponse);
+  }
+}
+
+const projectEvents = new ProjectEvents();
+module.exports = projectEvents;
