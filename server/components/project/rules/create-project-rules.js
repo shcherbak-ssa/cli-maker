7530e7dfcce41fb4865b8e3c755d15e632173c14
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-03 17:30:00 +0300

    draft: create-project-rules class

---
+++ server/components/project/rules/create-project-rules.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class CreateProjectRules {
+  
+  async check(projectName) {
+
+  }
+
+  _isOverMaxLength(projectName) {}
+  _isContainsInvalidSymbols(projectName) {}
+}
+
+module.exports = CreateProjectRules;
