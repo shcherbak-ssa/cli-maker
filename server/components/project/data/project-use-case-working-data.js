e663a3c3484fdc900ba347460ef1d83af540fa52
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-03 14:12:28 +0300

    create: project-use-case-working-data classes

---
+++ server/components/project/data/project-use-case-working-data.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const ProjectDataCreator = require('./project-data');
+
+class ProjectUseCaseWorkingDataType {
+  constructor() {
+    this.accessID = '';
+    this.data = {};
+  }
+}
+
+class ProjectUseCaseWorkingData {
+  constructor(projectUseCaseWorkingDataType) {
+    this._data = projectUseCaseWorkingDataType;
+  }
+
+  getAccessID() {
+    return this._accessID;
+  }
+  getProjectData() {
+    return this._data.data;
+  }
+}
+
+class ProjectUseCaseWorkingDataCreator {
+  constructor() {
+    this._data = new ProjectUseCaseWorkingDataType();
+  }
+
+  setAccessID(accessID) {
+    this._data.accessID = accessID;
+    return this;
+  }
+  setProjectData(data) {
+    const {name, id} = data;
+    const creator = new ProjectDataCreator();
+
+    this._data.data = creator
+      .setName(name)
+      .setProjectID(id)
+      .getProjectData();
+
+    return this;
+  }
+  getWorkingData() {
+    return new ProjectUseCaseWorkingData(this._data);
+  }
+}
+
+module.exports = ProjectUseCaseWorkingDataCreator;
