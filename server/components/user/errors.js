2d162f4312852cf4b683c407d79563009f7e430b
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-03 10:43:05 +0300

    refactor: user component errors

--- server/components/user/errors.js
+++ server/components/user/errors.js
@@ -2,26 +2,27 @@
 
 const userResponseWorker = require('./data/user-response-worker');
 
+function createErrorResponse(type, message) {
+  return userResponseWorker.createErrorResponse(type, message);
+}
+
 class UserError extends Error {
   constructor(message) {
     super(message);
     this.name = 'UserError';
-    this.errorType = '';
-    this.userResponse = userResponseWorker
-      .createErrorResponse(this.errorType, message);
   }
 }
 
 class UserServerError extends UserError {
   constructor(message) {
     super(message);
-    this.errorType = 'Server error'
+    this.userResponse = createErrorResponse('Server error', message);
   }
 }
 class UserDoesNotExist extends UserError {
   constructor(message) {
     super(message);
-    this.errorType = 'Action error'
+    this.userResponse = createErrorResponse('Action error', message);
   }
 }
 
