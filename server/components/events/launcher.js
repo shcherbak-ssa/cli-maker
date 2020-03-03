3f3e02492d32c67e72a947b525926217cb83f681
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-03 14:45:36 +0300

    update: events component launcher

--- server/components/events/launcher.js
+++ server/components/events/launcher.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const userEvents = require('./user/user-events');
+const projectEvents = require('./project/project-events');
 
 function initEventsComponent() {
   userEvents.initUserEvents();
+  projectEvents.initProjectEvents();
 }
 
 module.exports = initEventsComponent;
