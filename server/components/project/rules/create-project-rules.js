a29bf9e800dc1ff91239e7f557b7efa13e73f173
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-03 18:01:12 +0300

    create: create-project-rules class

--- server/components/project/rules/create-project-rules.js
+++ server/components/project/rules/create-project-rules.js
@@ -1,13 +1,26 @@
 'use strict';
 
+const {projectRules, errorMessages} = require('./project-rules');
+const {ProjectRulesError} = require('../errors');
+
 class CreateProjectRules {
+  _maxProjectNameLength = projectRules.maxProjectNameLength;
+  _invalidSymbols = projectRules.invalidSymbols;
   
   async check(projectName) {
-
+    if( this._isOverMaxLength(projectName) )
+      throw new ProjectRulesError(errorMessages.overMaxLength);
+    
+    if( this._isContainsInvalidSymbols(projectName) )
+      throw new ProjectRulesError(errorMessages.invalidSymbol);
   }
 
-  _isOverMaxLength(projectName) {}
-  _isContainsInvalidSymbols(projectName) {}
+  _isOverMaxLength(projectName) {
+    return projectName.length > this._maxProjectNameLength;
+  }
+  _isContainsInvalidSymbols(projectName) {
+    return this._invalidSymbols.test(projectName);
+  }
 }
 
 module.exports = CreateProjectRules;
--- server/components/project/rules/project-rules.js
+++ server/components/project/rules/project-rules.js
@@ -4,5 +4,12 @@ const projectRules = {
   maxProjectNameLength: 24,
   invalidSymbols: /`~!#\$%\^&\*\(\)\+=;:'\"\\\|\/,{}\[]/
 };
+const errorMessages = {
+  overMaxLength: `project name cannot exceed ${projectRules.maxProjectNameLength} characters`,
+  invalidSymbol: `project name may contains numbers, letters and symbols: @, _, -,`
+};
 
-module.exports = projectRules;
+module.exports = {
+  projectRules,
+  errorMessages
+};
