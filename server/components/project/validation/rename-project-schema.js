d7cb64bf496c0e935db22cb5ff97533fd17e5454
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-04 13:31:31 +0300

    create: rename-project validation schema

--- server/components/project/validation/rename-project-schema.js
+++ server/components/project/validation/rename-project-schema.js
@@ -4,18 +4,20 @@ const projectNameSchema = require('./src/project-name-schema');
 const projectIDSchema = require('./src/project-id-schema');
 
 const renameProjectSchema = {
-  $merge: {
+  $patch: {
     source: {
       type: 'object',
+      required: ['name', 'id'],
       properties: {},
       additionalProperties: false,
       errorMessage: {
-        additionalProperties: 'should not have properties other than \'id\''
+        additionalProperties: 'should not have properties other than \'name\' and \'id\''
       }
     },
-    with: {
-      ...projectIDSchema
-    }
+    with: [
+      ...projectIDSchema,
+      ...projectNameSchema
+    ]
   }
 };
 
