66df456f7667c82416fa531ac3eb6ded11859409
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-04 12:55:16 +0300

    update: project component rules

---
+++ server/components/project/rules/project-id-rules.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const PROJECT_ID_TYPE = 'string';
+const MAX_PROJECT_ID_LENGTH = 30;
+const PROJECT_ID_PATTERN = '[-\w]+';
+
+module.exports = {
+  PROJECT_ID_TYPE,
+  MAX_PROJECT_ID_LENGTH,
+  PROJECT_ID_PATTERN
+};
---
+++ server/components/project/rules/project-name-rules.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const PROJECT_NAME_TYPE = 'string';
+const MAX_PROJECT_NAME_LENGTH = 24;
+const PROJECT_NAME_PATTERN = '[-@\w]+';
+
+module.exports = {
+  PROJECT_NAME_TYPE,
+  MAX_PROJECT_NAME_LENGTH,
+  PROJECT_NAME_PATTERN
+};
