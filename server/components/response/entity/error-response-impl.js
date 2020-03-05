bda2b6553b6d0eaa54122636010759b32ee3f25e
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-05 16:14:16 +0300

    update: add implementations in error-response-impl

--- server/components/response/entity/error-response-impl.js
+++ server/components/response/entity/error-response-impl.js
@@ -2,6 +2,9 @@
 
 const errorEntityResponse = require('./src/error-entity-response');
 
+const IUserErrorResponse = require('../../user/response/i-user-error-response');
+const IProjectErrorResponse = require('../../project/response/i-project-error-response');
+
 class ErrorResponseImpl {
   async create(type, message) {
     return await errorEntityResponse.create(type, message);
@@ -10,6 +13,8 @@ class ErrorResponseImpl {
 
 function implementErrorResponses() {
   const errorResponseImpl = new ErrorResponseImpl();
+  IUserErrorResponse.create = errorResponseImpl.create;
+  IProjectErrorResponse.create = errorResponseImpl.create;
 }
 
 module.exports = implementErrorResponses;
