4052c38a7ee879d69336679f8bb603cb90b126c2
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-05 16:16:21 +0300

    update: added implementations in success-response-impl

--- server/components/response/entity/success-response-impl.js
+++ server/components/response/entity/success-response-impl.js
@@ -2,6 +2,9 @@
 
 const successEntityResponse = require('./src/success-entity-response');
 
+const IUserSuccessResponse = require('../../user/response/i-user-success-response');
+const IProjectSuccessResponse = require('../../project/response/i-project-success-response');
+
 class SuccessResponseImpl {
   async create(data, responseType) {
     return await successEntityResponse.create(data, responseType);
@@ -10,6 +13,8 @@ class SuccessResponseImpl {
 
 function implementSuccessResponses() {
   const successResponseImpl = new SuccessResponseImpl();
+  IUserSuccessResponse.create = successResponseImpl.create;
+  IProjectSuccessResponse.create = successResponseImpl.create;
 }
 
 module.exports = implementSuccessResponses;
