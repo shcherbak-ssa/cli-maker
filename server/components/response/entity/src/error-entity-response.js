3ba8517adc376d12b3a2b11ea16bee346f22381b
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-03-05 14:36:20 +0300

    update: error-entity-response class

--- server/components/response/entity/src/error-entity-response.js
+++ server/components/response/entity/src/error-entity-response.js
@@ -1,14 +1,39 @@
 'use strict';
 
+const responseCache = require('../../cache');
 const EntityResponseCreator = require('./entity-response');
+const jsonResponseObjectCreator = require('../../object/json-response-object-creator');
 
-function createErrorEntityResponse(type, message) {
-  const creator = new EntityResponseCreator();
-  return creator
-    .setErrorFlag(true)
-    .setType(type)
-    .setMessage(message)
-    .getResponse();
+class ErrorEntityResponse {
+  async create(data, responseType) {
+    try {
+      return await this._tryToCreate(data, responseType);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async _tryToCreate(data, responseType) {
+    const entityResponse = this._createEntityResponse(data);
+    const responseObject = await this._createResponseObject(entityResponse);
+    const responseID = this._saveResponseObjectInCache(responseObject);
+    return responseID;
+  }
+  _createEntityResponse(type, message) {
+    const creator = new EntityResponseCreator();
+    return creator
+      .setErrorFlag(true)
+      .setType(type)
+      .setMessage(message)
+      .getResponse();
+  }
+  async _createResponseObject(entityResponse) {
+    return await jsonResponseObjectCreator.create(entityResponse);
+  }
+  _saveResponseObjectInCache(responseObject) {
+    return responseCache.addResponseObject(responseObject);
+  }
 }
 
-module.exports = createErrorEntityResponse;
+const errorEntityResponse = new ErrorEntityResponse();
+module.exports = errorEntityResponse;
